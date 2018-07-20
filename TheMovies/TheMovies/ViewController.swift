69b19d3e3099942f4e7a1fba73841e1feb4bb1ae
intern-Dong <trinh.giang.dong@framgia.com>
2018-07-20 17:28:16 +0700

    create project

---
+++ TheMovies/TheMovies/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TheMovies
+//
+//  Created by Trịnh Giang Đông on 7/20/18.
+//  Copyright © 2018 TrinhGiangDong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
