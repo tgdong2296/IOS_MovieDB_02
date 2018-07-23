478ba2e260aec7d9c4201b7b74861779ab7ab838
Hai <hai@Hais-MacBook-Pro.local>
2018-07-23 16:55:56 +0700

    reinitProject

---
+++ TheMoviesReal/TheMoviesReal/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TheMoviesReal
+//
+//  Created by Hai on 7/23/18.
+//  Copyright © 2018 Hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
